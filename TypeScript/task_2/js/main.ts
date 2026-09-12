63b245feec40b0d35819c1af0ff8f0450d6dafb2
Trevnh <trevorhuynh@hotmail.com>
2026-09-12 20:13:58 +1000

    Add Subjects type and teachClass function

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -63,3 +63,17 @@ function executeWork(employee: Director | Teacher): string {
 
 console.log(executeWork(createEmployee(200)));
 console.log(executeWork(createEmployee(1000)));
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects):string {
+    if (todayClass === "Math") {
+        return ('Teaching Math');
+    }
+    if (todayClass === "History") {
+        return ('Teaching History');
+    }
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
